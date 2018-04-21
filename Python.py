fed3888138f8d6e9dda5e9bb6907fa789b9ae552
natvon13 <38588935+natvon13@users.noreply.github.com>
2018-04-21 12:45:21 -0400

    First Python Code EVER

---
+++ Python.py
@@ -0,0 +1,17 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============================== RESTART: Shell ===============================
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print "hello"
+hello
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print "shlabalabadingdong"
+shlabalabadingdong
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print "Natalia"
+Natalia
+>>> 
